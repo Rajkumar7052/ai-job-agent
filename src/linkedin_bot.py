9b189291976a49e26391cf5fc339a624ed2637e3
Raj <rajkumarmaurya@gmail.com>
2025-07-29 10:40:03 +0530

    updated linkdin bot

--- src/linkedin_bot.py
+++ src/linkedin_bot.py
@@ -12,7 +12,7 @@ class LinkedInBot:
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=False)
             page = browser.new_page()
-            page.goto("https://www.linkedin.com/login")
+            page.goto("https://www.linkedin.com/in/raj-maurya-gcp-data-engineer/")
 
             page.fill("input#username", self.username)
             page.fill("input#password", self.password)
@@ -22,12 +22,12 @@ class LinkedInBot:
             page.goto("https://www.linkedin.com/jobs")
             page.fill("input[aria-label='Search jobs']", job_title)
             page.keyboard.press("Enter")
-            page.wait_for_timeout(5000)
+            page.wait_for_timeout(10000)
 
             jobs = page.query_selector_all("ul.jobs-search__results-list li")
             for job in jobs[:3]:
                 job.click()
-                page.wait_for_timeout(2000)
+                page.wait_for_timeout(20000)
                 job_desc = page.inner_text("div.description")
                 modified_resume = self.resume_modifier.modify(job_desc)
                 self.job_logger.log(job_desc)
